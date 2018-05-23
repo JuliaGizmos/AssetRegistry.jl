1ea77228fc4eee29dff4079aa9d0d4a521cbcb31
ranjanan <benditlikeranjan@gmail.com>
2018-05-23 14:13:03 +0530

    Initial commit

--- src/AssetRegistry.jl
+++ src/AssetRegistry.jl
@@ -1,5 +1,12 @@
 module AssetRegistry
 
-# package code goes here
+const registry = Dict{String,String}()
+
+function register(path)
+    key = sha1(abspath(path))
+    registry[key] = abspath(path) 
+    key
+end
+
 
 end # module
