142c60448b8ceca262caf24491b9ec9a5fc87198
Bart Janssens <bart@bartjanssens.org>
2018-08-28 11:51:13 +0200

    Add baseurl to prefix all paths with

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,21 @@
-using AssetRegistry
-ps = addprocs(1)
-
+ENV["JULIA_ASSETREGISTRY_BASEURL"] = "/mybasedir"
 @static if VERSION < v"0.7.0-DEV.2005"
     using Base.Test
 else
+    using Distributed
     using Test
 end
+using AssetRegistry
+ps = addprocs(1)
 
 using JSON
 
 @testset "register" begin
     key = AssetRegistry.register(pwd())
+    @test startswith(key, ENV["JULIA_ASSETREGISTRY_BASEURL"])
     @test key == AssetRegistry.getkey(pwd())
     @test AssetRegistry.isregistered(pwd())
-    @test JSON.parse(String(read(joinpath(homedir(), ".jlassetregistry.json")))) == Dict(key => [pwd(), 1])
+    @test JSON.parse(String(read(joinpath(homedir(), ".jlassetregistry.json")))) == Dict(AssetRegistry.filekey(key) => [pwd(), 1])
 
     # test that a new proc doesn't leave any residue
     run(```
@@ -21,7 +23,7 @@ using JSON
         -e 'using AssetRegistry; AssetRegistry.register(pwd());'
         ```)
 
-    @test JSON.parse(String(read(joinpath(homedir(), ".jlassetregistry.json")))) == Dict(key => [pwd(), 1])
+    @test JSON.parse(String(read(joinpath(homedir(), ".jlassetregistry.json")))) == Dict(AssetRegistry.filekey(key) => [pwd(), 1])
 
     AssetRegistry.deregister(pwd())
     @test !AssetRegistry.isregistered(pwd())
