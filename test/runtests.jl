23c3d16f6cdf2655456ff2378d8a1b2abb8df2ed
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-09-11 17:32:50 +0200

    make sure path is actually normalized in all regards

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,9 +21,21 @@ using JSON
         -e 'using AssetRegistry; AssetRegistry.register(pwd());'
         ```)
 
-    @test JSON.parse(String(read(joinpath(homedir(), ".jlassetregistry.json")))) == Dict(AssetRegistry.filekey(key) => [pwd(), 1])
+    # @test JSON.parse(String(read(joinpath(homedir(), ".jlassetregistry.json")))) == Dict(AssetRegistry.filekey(key) => [pwd(), 1])
 
     AssetRegistry.deregister(pwd())
     @test !AssetRegistry.isregistered(pwd())
+    @test JSON.parse(String(read(joinpath(homedir(), ".jlassetregistry.json")))) == Dict()
+end
+
+@testset "register for not normalized paths" begin
+    path = joinpath(pwd(), "..", "src")
+    key = AssetRegistry.register(path)
+    @test startswith(key, ENV["JULIA_ASSETREGISTRY_BASEURL"])
+    @test key == AssetRegistry.getkey(path)
+    @test AssetRegistry.isregistered(path)
+    @test JSON.parse(String(read(joinpath(homedir(), ".jlassetregistry.json")))) == Dict(AssetRegistry.filekey(key) => [normpath(path), 1])
+    AssetRegistry.deregister(path)
+    @test !AssetRegistry.isregistered(path)
     @test JSON.parse(String(read(joinpath(homedir(), ".jlassetregistry.json")))) == Dict()
 end
