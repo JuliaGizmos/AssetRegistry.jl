b3bff7d0479af0d028647c663948aec4d2e6feb2
rafaqz <rafaelschouten@gmail.com>
2022-01-23 14:02:10 +0100

    refactor and precompile

--- src/AssetRegistry.jl
+++ src/AssetRegistry.jl
@@ -26,7 +26,7 @@ julia> key = AssetRegistry.register("/Users/ranjan/.julia/v0.6/Tachyons/assets/t
 ```
 """
 function register(path; registry_file = joinpath(homedir(), ".jlassetregistry.json"))
-    target = normpath(abspath(expanduser(path)))
+    target = gettarget(path)
     (isfile(target) || isdir(target)) || error("Asset not found")
 
     key = getkey(target)
@@ -36,64 +36,29 @@ function register(path; registry_file = joinpath(homedir(), ".jlassetregistry.js
     end
 
     # update global registry file
-
-    # touch(registry_file) -- this doesn't work on Azure fs
-    if !isfile(registry_file)
-        # WARN: may need a lock here
-        open(_->nothing, registry_file, "w")
-    end
-
-    pidlock = joinpath(homedir(), ".jlassetregistry.lock")
-
-    withlock(pidlock) do
-        fkey = filekey(key)
-        io = open(registry_file, "r+") # open in read-and-write mode
-        # first get existing entries
-        DT = Dict{String,Tuple{String,Int}}
-        prev_registry =  filesize(io) > 0 ? JSON3.read(io, DT) : DT()
-
+    update_registry_file(registry_file, key) do prev_registry, fkey
         if haskey(prev_registry, fkey)
             prev_registry[fkey] = (target, prev_registry[fkey][2]+1) # increment ref count
         else
             prev_registry[fkey] = (target, 1) # init refcount to 1
         end
-
-        # write the updated registry to file
-        seekstart(io)
-        JSON3.write(io, prev_registry)
-
-        # truncate it to current length
-        truncate(io, position(io))
-        close(io)
     end
 
     registry[key] = target # keep this in current process memory
 
-
-    key
+    return key
 end
 
 function deregister(path; registry_file = joinpath(homedir(), ".jlassetregistry.json"))
-    target = normpath(abspath(expanduser(path)))
+    target = gettarget(path)
 
     key = getkey(target)
     if !haskey(registry, key)
-        return
+        return nothing
     end
-
     pop!(registry, key)
 
-    touch(registry_file)
-
-    pidlock = joinpath(homedir(), ".jlassetregistry.lock")
-    withlock(pidlock) do
-        fkey = filekey(key)
-        io = open(registry_file, "r+")
-
-        # get existing information
-        DT = Dict{String,Tuple{String,Int}}
-        prev_registry =  filesize(io) > 0 ? JSON3.read(io, DT) : DT()
-
+    update_registry_file(registry_file, key) do prev_registry, fkey
         if haskey(prev_registry, fkey)
             val, count = prev_registry[fkey]
             if count == 1
@@ -102,17 +67,35 @@ function deregister(path; registry_file = joinpath(homedir(), ".jlassetregistry.
                 prev_registry[fkey] = (val, count-1) # increment ref count
             end
         end
+    end
 
+    return key
+end
+
+function update_registry_file(f, file, key)
+    pidlock = joinpath(homedir(), ".jlassetregistry.lock")
+    # touch(registry_file) -- this doesn't work on Azure fs
+    if !isfile(file)
+        # WARN: may need a lock here
+        open(_ -> nothing, file, "w")
+    end
+    withlock(pidlock) do
+        fkey = filekey(key)
+        io = open(file, "r+")
+        # get existing information
+        DT = Dict{String,Tuple{String,Int}}
+        prev_registry =  filesize(io) > 0 ? JSON3.read(io, DT) : DT()
+        f(prev_registry, fkey)
+        # write the updated registry to file
         seekstart(io)
         JSON3.write(io, prev_registry)
-
         # truncate it to current length
         truncate(io, position(io))
         close(io)
     end
-
-    key
 end
+
+gettarget(path) = normpath(abspath(expanduser(path)))
 
 getkey(path) =  baseurl[] * "/assetserver/" * bytes2hex(sha1(abspath(path))) * "-" * basename(path)
 
@@ -125,6 +108,12 @@ function __init__()
             AssetRegistry.deregister(path)
         end
     end
+end
+
+# Precompile with a dummy registration
+let
+    register(pwd())
+    deregister(pwd())
 end
 
 end # module
