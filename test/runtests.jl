ca39ec9a4ca02a5d5bd9af8736d3262b627dfb68
Helmut Hänsel <helmut.haensel@gmx.de>
2025-10-15 00:33:20 +0200

    adapt tests to JSON v1 and Julia v1.12

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,35 +7,41 @@ ps = addprocs(1)
 using Test
 
 using JSON
+import AssetRegistry.parsejson
+
+# julia 1.12 precompiles in a separate process with in a different directory,
+# so we need to use `@__DIR__` insted of `pwd()`
+mydir = Base.@__DIR__
+registry_file = joinpath(homedir(), ".jlassetregistry.json")
 
 @testset "register" begin
-    key = AssetRegistry.register(pwd())
+    key = AssetRegistry.register(mydir)
     @test startswith(key, ENV["JULIA_ASSETREGISTRY_BASEURL"])
-    @test key == AssetRegistry.getkey(pwd())
-    @test AssetRegistry.isregistered(pwd())
-    @test JSON.parse(String(read(joinpath(homedir(), ".jlassetregistry.json")))) == Dict(AssetRegistry.filekey(key) => [pwd(), 1])
+    @test key == AssetRegistry.getkey(mydir)
+    @test AssetRegistry.isregistered(mydir)
+    @test parsejson(String(read(registry_file))) == Dict(AssetRegistry.filekey(key) => [mydir, 1])
 
     # test that a new proc doesn't leave any residue
     run(```
         $(Base.julia_cmd())
-        -e 'using AssetRegistry; AssetRegistry.register(pwd());'
+        --project=@. -e 'using AssetRegistry; AssetRegistry.register(pwd());'
         ```)
 
-    @test JSON.parse(String(read(joinpath(homedir(), ".jlassetregistry.json")))) == Dict(AssetRegistry.filekey(key) => [pwd(), 1])
+    @test parsejson(String(read(registry_file))) == Dict(AssetRegistry.filekey(key) => [mydir, 1])
 
-    AssetRegistry.deregister(pwd())
-    @test !AssetRegistry.isregistered(pwd())
-    @test JSON.parse(String(read(joinpath(homedir(), ".jlassetregistry.json")))) == Dict()
+    AssetRegistry.deregister(mydir)
+    @test !AssetRegistry.isregistered(mydir)
+    @test parsejson(String(read(registry_file))) == Dict()
 end
 
 @testset "register for not normalized paths" begin
-    path = joinpath(pwd(), "..", "src")
+    path = joinpath(mydir, "..", "src")
     key = AssetRegistry.register(path)
     @test startswith(key, ENV["JULIA_ASSETREGISTRY_BASEURL"])
     @test key == AssetRegistry.getkey(path)
     @test AssetRegistry.isregistered(path)
-    @test JSON.parse(String(read(joinpath(homedir(), ".jlassetregistry.json")))) == Dict(AssetRegistry.filekey(key) => [normpath(path), 1])
+    @test parsejson(String(read(registry_file))) == Dict(AssetRegistry.filekey(key) => [normpath(path), 1])
     AssetRegistry.deregister(path)
     @test !AssetRegistry.isregistered(path)
-    @test JSON.parse(String(read(joinpath(homedir(), ".jlassetregistry.json")))) == Dict()
+    @test parsejson(String(read(registry_file))) == Dict()
 end
